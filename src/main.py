41cd8ae93f6bf149fcfa07379cf67d4c9bba05cd
Seseel Lybon <Seseellybon@gmail.com>
2019-01-03 13:38:57 +0100

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -28,34 +28,39 @@ print("start")
 #SuperiourGE(10**6)
 print("-----")
 #temp = ClassifiedGE(10**5)
-#temp = ClassifiedGE(10**5, 2)
 
-import cProfile
-import re
-cProfile.run('SuperiourGE(10**5)')
 
-'''
+temp = ClassifiedGE(10**4, 2)
+
+
+do_profile = False
+if do_profile:
+    import cProfile
+    import re
+    cProfile.run('SuperiourGE(10**3)')
+
+
 
 print("Done")
 
-temp = numpy.sort(temp, kind = 'mergesort')
-
-temp = compression(temp)
 
 
 
 
+do_graph = False
+if do_profile:
 
+    print("Plotting...")
 
-print("Plotting...")
+    temp = numpy.sort(temp, kind = 'mergesort')
 
+    temp = compression(temp)
 
-fig = plt.figure()
+    fig = plt.figure()
+    #
+    #plt.scatter(range(len(temp)), temp)
+    #plt.plot(temp)
 
+    plt.bar(range(len(temp)), temp)
 
-#plt.scatter(range(len(temp)), temp)
-#plt.plot(temp)
-plt.bar(range(len(temp)), temp)
-
-plt.show()
-'''
+    plt.show()
