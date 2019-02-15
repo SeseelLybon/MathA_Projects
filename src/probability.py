d60bc36e269534b0456f9871a754186de779f6c6
Seseel Lybon <Seseellybon@gmail.com>
2019-02-15 01:29:11 +0100

    Major progres
    Still don't understand anything about probability, though

--- src/probability.py
+++ src/probability.py
@@ -5,6 +5,10 @@ from externs import State
 
 from collections import Counter
 
+def ftpt(x):
+    return round( x*100, 2)
+
+
 def chances(throw):
     throw = sorted(throw)
     states = check_throw(throw)
@@ -14,46 +18,64 @@ def chances(throw):
 
     if State.yatzee not in states:
         print("\t\t Chance of a yatzee:")
+
+        # p(a)*4
         if len( cntr.values()) == 5:
-            print("\t\t 4:", round( (1/6)**4 *100, 2))
+            print("\t\t 4:", ftpt( (1/6)**4 ))
+        # p(a)*3
         if 2 in cntr.values():
-            print("\t\t 3:", round( (1/6)**3 *100, 2))
+            print("\t\t 3:", ftpt( (1/6)**3 ))
+        # p(a)*2
         if 3 in cntr.values():
-            print("\t\t 2:", round( (1/6)**2 *100, 2))
+            print("\t\t 2:", ftpt( (1/6)**2 ))
+        # p(a)
         if 4 in cntr.values():
-            print("\t\t 1:", round( (1/6) *100, 2))
+            print("\t\t 1:", ftpt( (1/6) ))
 
 
     if State.full_house not in states:
         print("\t\t Chance of a full house:")
         # contains a b c d e
-        # needs a && b && ( a || b )
+        # needs 2a && b
+        # p(a) and p(a) and p(b) = (3/6)*(3/6)*(2/6)*(1/6)
         # roll c d e
         if len( cntr.values()) == 5:
-            print("\t\t a b c d e")
-            print("\t\t 3:", round( (1/6)*(1/6)*(2/6) *100, 2))
+            print("\t\t a b c d e -> c d e")
+            print("\t\t 3:", ftpt( (3/6)*(3/6)*(2/6)*(1/6) ))
 
         # contains 2a 2b c
         # needs a || b
+        # p(a) or p(b) = (1/6)+(1/6)
         # roll c
-        if list(cntr.values()).count(2):
-            print("\t\t 2a 2b c")
-            print("\t\t 1:", round( (2/6) *100, 2))
+        if list(cntr.values()).count(2) == 2:
+            print("\t\t 2a 2b c -> c")
+            print("\t\t 1:", ftpt( (1/6)+(1/6) ))
+
+        # contains 2a b c d
+        # needs a || b
+        # p(bbb) =
+        # roll b c d
+        if list(cntr.values()).count(2) == 1 and 3 not in cntr.values():
+            print("\t\t", "2a b c d", "->", "b c d")
+            print("\t\t 3:", ftpt( (1/6)**3 ))
 
         # contains a b4 | 3a b c
         # needs a       | b
+        # p( a ) = (1/6)
         # roll b        | c
         if 4 in cntr.values() or \
                 3 in cntr.values() and 2 not in cntr.values():
-            print("\t\t a b4 || 3a b c")
-            print("\t\t 1:", round( (1/6) *100, 2))
+            print("\t\t a b4 -> b || 3a b c -> c")
+            print("\t\t 1:", ftpt( (1/6) ))
 
         # contains 5a
         # need 2b
+        # P(b) and p(b) = (1/6)*(1/6)
         # roll 2a
         if 5 in cntr.values():
-            print("\t\t 5a")
-            print("\t\t 2:", round( (5/6)*(1/6) *100, 2))
+            print("\t\t 5a -> 2a")
+            print("\t\t 2:", ftpt( (1/6)*(1/6) ))
+
 
 
     if State.long_straight not in states:
@@ -62,61 +84,147 @@ def chances(throw):
 
             # contains 1 2 3 4 6 | 1 3 4 5 6
             # needs 5            | 2
+            # p(5) = (1/6)
             # roll 6             | 1
             if 1 in throw or 6 in throw:
-                print("\t\t 1 2 3 4 6 || 1 3 4 5 6")
-                print("\t\t 1:", round( (1/6) *100, 2) )
+                print("\t\t 1 2 3 4 6 -> 6|| 1 3 4 5 6 -> 1")
+                print("\t\t 1:", ftpt( (1/6) ))
             # contains 2 3 4 5 a
             # needs 1 || 6
+            # P(1 or 6) = (1/6+1/6)
             # roll a
             else:
-                print("\t\t 2 3 4 5 a")
-                print("\t\t 1:", round( (2/6) *100, 2) )
+                print("\t\t 2 3 4 5 a -> a")
+                print("\t\t 1:", ftpt( 1-(4/6)**2 ))
 
-        # contains 2a 2b c | 3a b c
-        # needs d e        | d e
-        # roll a b         | 2a
-        if list(cntr.values()).count(2) or \
-                3 in cntr.values() and 2 not in cntr.values():
-            print("\t\t 2a 2b c || 3a b c")
-            print("\t\t 2:", round( (1/6)*(2/6) *100, 2))
+
+        # contains 2a 2b c
+        # needs d && e
+        # p(d) and p(e) = (1/6)*(1/6)
+        # roll a b
+        if list(cntr.values()).count(2) == 2:
+            print("\t\t 2a 2b c -> a b")
+            print("\t\t 2:", ftpt( (2/6)*(1/6) ))
+
+        # contains 3a b c
+        # needs d && e
+        # p(d) and p(e) = (2/6)*(1/6)
+        # roll 2a
+        if 3 in cntr.values() and 2 not in cntr.values():
+            print("\t\t", "3a b c", "->", "2a")
+            print("\t\t 2:", ftpt( (2/6)*(1/6) ))
+
+
+
+        # contains 3a b c with c-a <=4
+        # needs d && e
+        # p(d) and p(e) = (2/6)*(1/6)
+        # roll 2a
+        if 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] <= 4:
+            print("\t\t 3a b c with c-a <=4", "->", "2a")
+            print("\t\t 2:", ftpt( (2/6)*(1/6) ))
+        # contains 3a b c with c-a > 4
+        # needs b && c && d
+        # p(b and c and d) = (3/6)*(2/6)*(1/6)
+        # roll 2a c
+        elif 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] > 4:
+            print("\t\t", "3a b c with c-a > 4", "->", "2a c")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(1/6) ))
+
+        # contains 3a 2b with c-a <= 4
+        # needs c %% d %% e
+        # p(c) and p(d) and p(e) = (3/6)*(2/6)*(1/6)
+        # roll 2a 1b
+        if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] <= 4:
+            print("\t\t", "3a 2b with c-a <= 4", "->", "2a 2b")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(1/6) ))
+        # contains 3a 2b %% b-a > 4
+        # needs b %% c %% d % e
+        # p( b and c and d and e ) = (4/6)*(3/6)*(2/6)*(1/6)
+        # roll 2a 2b
+        if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] > 4:
+            print("\t\t", "3a 2b %% b-a <= 4", "->", "2a 2b || 3a b")
+            print("\t\t 4:", ftpt( (4/6)*(3/6)*(2/6)*(1/6) ))
 
         # contains a b4
-        # needs c && d && e [and !6]
+        # needs c && d && e
+        # p( c and d and e ) = (3/6)*(2/6)*(1/6)
         # roll 3b
         if 4 in cntr.values():
-            print("\t\t a b4")
-            print("\t\t 3:", round( (3/6)*(2/6)*(1/6) *100, 2))
+            print("\t\t a b4 -> 3b")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(1/6) ))
 
         # contains 5a
-        # need b c d e
+        # need b && c && d && e
+        # p( b and c and d and e ) = (4/6)*(3/6)*(2/6)*(1/6)
         # roll 4a
         if 5 in cntr.values():
-            print("\t\t 5a")
-            print("\t\t 4:", round( (4/6)*(3/6)*(2/6)*(1/6) *100, 2))
+            print("\t\t 5a -> 4a")
+            print("\t\t 4:", ftpt( (4/6)*(3/6)*(2/6)*(1/6) ))
+
 
     if State.short_straight not in states:
         print("\t\t Chance of a short_straight:")
-        if State.short_straight in states:
-            pass
-        # contains 2a 2b c  | 3a b c
-        # needs d           | d
-        # roll a b          | 2a
-        if list(cntr.values()).count(2) or \
-                3 in cntr.values() and 2 not in cntr.values():
-            print("\t\t 2a 2b c || 3a b c")
-            print("\t\t 2:", round( (1-(5/6)*(5/6)) *100, 2))
+
+
+        # contains 3a b c with c-a <=3
+        # needs d
+        # p( d ) = 1/6+1/6
+        # roll 2a
+        if 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] <= 3:
+            print("\t\t 3a b c with c-a <=3", "->", "2a")
+            print("\t\t 2:", ftpt( 1/6+1/6 ))
+        # contains 3a b e with e-a > 3
+        # needs c %% d
+        # p(c) and p(d) = (3/6)*(2/6)*(5/6)
+        # roll 2a e
+        elif 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] > 3:
+            print("\t\t", "3a b c with c-a > 3", "->", "2a c")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(4/6) ))
+
+        # contains 3a 2b with c-a <= 3
+        # needs c && d
+        # p( c and d ) = (3/6)*(2/6)*(5/6)
+        # roll 2a 1b
+        if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] <= 3:
+            print("\t\t", "3a 2b with c-a <= 3", "->", "2a 1b")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(5/6) ))
+        # contains 3a 2e %% e-a > 3
+        # needs b %% c %% d
+        # p( b and c and d ) = (3/6)*(3/6)*(2/6)
+        # roll 2a 2e
+        if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] > 3:
+            print("\t\t", "3a 2b %% b-a <= 3", "->", "2a 2b || 3a b (Might be wrong)")
+            print("\t\t 4:", ftpt( (3/6)*(3/6)*(2/6) ))
+
+
+        # contains 2a 2b c
+        # needs d
+        # p(d) = (1/6)+(1/6)
+        # roll a b
+        if list(cntr.values()).count(2) == 2 and throw[-1] - throw[1] <= 3:
+            print("\t\t 2a 2b c -> a b")
+            print("\t\t 2:", ftpt( (1/6)+(1/6) ))
+        # contains 2a 2b c
+        # needs c && d
+        # p(c) and p(d) = (3/6)*(2/6)
+        # roll a b c
+        elif list(cntr.values()).count(2) == 2 and throw[-1] - throw[1] > 3:
+            print("\t\t 2a 2b c -> a b c")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(4/6) ))
 
         # contains a b4
-        # needs c d
+        # needs c %% d
+        # p( c and d ) = (3/6)*(2/6)*(5/6)
         # roll 3b
         if 4 in cntr.values():
-            print("\t\t a b4")
-            print("\t\t 3:", round( (1-(4/6)*(4/6)*(4/6)) *100, 2))
+            print("\t\t a b4 -> 3b")
+            print("\t\t 3:", ftpt( (3/6)*(2/6)*(5/6) ))
 
         # contains 5a
-        # need b c d e
+        # need b %% c %% d
+        # p(b and c and d) = (3/6)*(3/6)*(2/6)
         # roll 4a
         if 5 in cntr.values():
-            print("\t\t 5a")
-            print("\t\t 4:", round( (4/6)*(3/6)*(2/6)*(1/6) *100, 2))
+            print("\t\t 5a -> 4a")
+            print("\t\t 4:", ftpt( (3/6)*(3/6)*(2/6) ))
--- src/yatzee_main.py
+++ src/yatzee_main.py
@@ -9,10 +9,32 @@ from player import Player
 from probability import chances
 
 from itertools import combinations_with_replacement
+from externs import State
 
 playerone = Player("playerone")
 
+'''
+thing = {
+        State.full_house: [],
+        State.short_straight: [],
+        State.long_straight: [],
+        State.yatzee: [],
+    }
 
+for throw in combinations_with_replacement(range(1,7), 5):
+    throw_states = check_throw(throw)
+    for state in throw_states:
+        if state in thing.keys():
+            thing[state].append(throw)
+
+for key, value in thing.items():
+    print(key)
+    for throw in value:
+        print("\t", throw)
+        chances(throw)
+'''
+
+'''
 for throw in combinations_with_replacement(range(1,7), 5):
 
     print(throw)
@@ -20,3 +42,20 @@ for throw in combinations_with_replacement(range(1,7), 5):
                                   check_throw(throw)
                                   ))
     print( chances(throw) )
+'''
+
+throw = None
+
+if not throw:
+    playerone.new_throw()
+    throw = playerone.throw
+else:
+    playerone.throw = throw
+    playerone.rethrow([0,1,1,0,1])
+    throw = playerone.throw
+
+print(sorted(throw))
+scoring.temp_repr(check_score(throw,
+                              check_throw(throw)
+                              ))
+print( chances(throw) )
