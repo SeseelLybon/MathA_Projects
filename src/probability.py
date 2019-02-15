dc617117a812dd0b577a08806cc9d6d90d9db6ba
Seseel Lybon <Seseellybon@gmail.com>
2019-02-15 14:30:54 +0100

    Minor changes

--- src/probability.py
+++ src/probability.py
@@ -37,27 +37,27 @@ def chances(throw):
         print("\t\t Chance of a full house:")
         # contains a b c d e
         # needs 2a && b
-        # p(a) and p(a) and p(b) = (3/6)*(3/6)*(2/6)*(1/6)
+        # p(a) and p(a) and p(b) = (1-5/6)*(1-5/6)*(1-4/6)
         # roll c d e
         if len( cntr.values()) == 5:
             print("\t\t a b c d e -> c d e")
-            print("\t\t 3:", ftpt( (3/6)*(3/6)*(2/6)*(1/6) ))
+            print("\t\t 3:", ftpt( (1-5/6)*(1-5/6)*(1-4/6) ))
 
         # contains 2a 2b c
         # needs a || b
-        # p(a) or p(b) = (1/6)+(1/6)
+        # p(a) or p(b) = (1-4/6)
         # roll c
         if list(cntr.values()).count(2) == 2:
             print("\t\t 2a 2b c -> c")
-            print("\t\t 1:", ftpt( (1/6)+(1/6) ))
+            print("\t\t 1:", ftpt( (1-4/6) ))
 
         # contains 2a b c d
-        # needs a || b
-        # p(bbb) =
-        # roll b c d
+        # needs a & b
+        # p(a and b) = (1/6)**2
+        # roll c d
         if list(cntr.values()).count(2) == 1 and 3 not in cntr.values():
-            print("\t\t", "2a b c d", "->", "b c d")
-            print("\t\t 3:", ftpt( (1/6)**3 ))
+            prtft("2a b c d", "b c d")
+            print("\t\t 2:", ftpt( (1/6)**2 ))
 
         # contains a b4 | 3a b c
         # needs a       | b
@@ -70,10 +70,10 @@ def chances(throw):
 
         # contains 5a
         # need 2b
-        # P(b) and p(b) = (1/6)*(1/6)
+        # P(b and b) = (1/6)*(1/6)
         # roll 2a
         if 5 in cntr.values():
-            print("\t\t 5a -> 2a")
+            prtft("5a", "2a")
             print("\t\t 2:", ftpt( (1/6)*(1/6) ))
 
 
@@ -91,16 +91,16 @@ def chances(throw):
                 print("\t\t 1:", ftpt( (1/6) ))
             # contains 2 3 4 5 a
             # needs 1 || 6
-            # P(1 or 6) = (1/6+1/6)
+            # P(1 or 6) = (1-5/6)
             # roll a
             else:
-                print("\t\t 2 3 4 5 a -> a")
-                print("\t\t 1:", ftpt( (2/6) ))
+                prtft("2 3 4 5 a", "a")
+                print("\t\t 1:", ftpt( (1-5/6) ))
 
 
         # contains 2a 2b c
         # needs d && e
-        # p(d) and p(e) = (1/6)*(1/6)
+        # p(d and e) = (2/6)*(1/6)
         # roll a b
         if list(cntr.values()).count(2) == 2:
             print("\t\t 2a 2b c -> a b")
@@ -108,7 +108,7 @@ def chances(throw):
 
         # contains 3a b c
         # needs d && e
-        # p(d) and p(e) = (2/6)*(1/6)
+        # p(d and e) = (2/6)*(1/6)
         # roll 2a
         if 3 in cntr.values() and 2 not in cntr.values():
             print("\t\t", "3a b c", "->", "2a")
@@ -118,33 +118,33 @@ def chances(throw):
 
         # contains 3a b c with c-a <=4
         # needs d && e
-        # p(d) and p(e) = (2/6)*(1/6)
+        # p(d and e) = (1-4/6)
         # roll 2a
         if 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] <= 4:
             print("\t\t 3a b c with c-a <=4", "->", "2a")
             print("\t\t 2:", ftpt( (2/6)*(1/6) ))
         # contains 3a b c with c-a > 4
         # needs b && c && d
-        # p(b and c and d) = (3/6)*(2/6)*(1/6)
+        # p(b and c and d) = (2/6)+(2/6)*(1/6)
         # roll 2a c
         elif 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] > 4:
             print("\t\t", "3a b c with c-a > 4", "->", "2a c")
-            print("\t\t 3:", ftpt( (3/6)*(2/6)*(1/6) ))
+            print("\t\t 3:", ftpt( (2/6)+(2/6)*(1/6) ))
 
         # contains 3a 2b with c-a <= 4
         # needs c %% d %% e
-        # p(c) and p(d) and p(e) = (3/6)*(2/6)*(1/6)
+        # p(c and d and e) = (2/6)+(2/6)*(1/6)
         # roll 2a 1b
         if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] <= 4:
             print("\t\t", "3a 2b with c-a <= 4", "->", "2a 2b")
             print("\t\t 3:", ftpt( (3/6)*(2/6)*(1/6) ))
         # contains 3a 2b %% b-a > 4
         # needs b %% c %% d % e
-        # p( b and c and d and e ) = (4/6)*(3/6)*(2/6)*(1/6)
+        # p( b and c and d and e ) = (2/6)+(2/6)*(1/6)
         # roll 2a 2b
         if 3 in cntr.values() and 2 in cntr.values() and throw[-1] - throw[1] > 4:
             print("\t\t", "3a 2b %% b-a <= 4", "->", "2a 2b || 3a b")
-            print("\t\t 4:", ftpt( (4/6)*(3/6)*(2/6)*(1/6) ))
+            print("\t\t 4:", ftpt( (2/6)+(2/6)*(1/6) ))
 
         # contains a b4
         # needs c && d && e
@@ -169,14 +169,14 @@ def chances(throw):
 
         # contains 3a b c with c-a <=3
         # needs d
-        # p( d ) = 1/6+1/6
+        # p( d ) = (1-5/6)**2
         # roll 2a
         if 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] <= 3:
             print("\t\t 3a b c with c-a <=3", "->", "2a")
-            print("\t\t 2:", ftpt( 1/6+1/6 ))
+            print("\t\t 2:", ftpt( (1-5/6)**2 ))
         # contains 3a b e with e-a > 3
         # needs c %% d
-        # p(c) and p(d) = (3/6)*(2/6)*(5/6)
+        # p(c and d) = (1-5/6)**2
         # roll 2a e
         elif 3 in cntr.values() and 2 not in cntr.values() and throw[-1] - throw[1] > 3:
             print("\t\t", "3a b c with c-a > 3", "->", "2a c")
@@ -200,18 +200,18 @@ def chances(throw):
 
         # contains 2a 2b c
         # needs d
-        # p(d) = (1/6)+(1/6)
+        # p(d) or p(d and e) = (1-4/6)+(2/6*1/6)
         # roll a b
         if list(cntr.values()).count(2) == 2 and throw[-1] - throw[1] <= 3:
-            print("\t\t 2a 2b c -> a b")
-            print("\t\t 2:", ftpt( (1/6)+(1/6) ))
+            prtft("2a 2b", "a b")
+            print("\t\t 2:", ftpt( (1-4/6)+(2/6*1/6) ))
         # contains 2a 2b c
         # needs c & d
-        # p(c) and p(d) = (3/6)*(2/6)*(4/6)
+        # p(c) and p(d) = 1/6+2/6*1/6
         # roll a b c
         elif list(cntr.values()).count(2) == 2 and throw[-1] - throw[1] > 3:
-            print("\t\t 2a 2b c -> a b c")
-            print("\t\t 3:", ftpt( (3/6)*(2/6)*(4/6) ))
+            prtft("2a 2b c", "a b c")
+            print("\t\t 3:", ftpt( 1/6+2/6*1/6 ))
 
         # contains a b4
         # needs c & d
